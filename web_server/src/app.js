a5d7c7fbc1441d19ced7e155ac2ecbf58c0fb5f7
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-05-25 17:11:18 +0530

    Update app.js

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,43 +1,44 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 
-// console.log(__dirname)
+// console.log(__dirname);
 // console.log(__filename)
 
-const publicDirectory = path.join(__dirname, '../static/public/')
+const publicDirectory = path.join(__dirname, "../static/public/");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const viewsPath = path.join(__dirname, "../templates/views")
+const viewsPath = path.join(__dirname, "../templates/views");
 
 // Set template engine to hbs
-app.set('view engine', 'hbs') 
-// tell the express where to look for template files other than default 'views' folder present at root folder 
-app.set('views', viewsPath)
+app.set("view engine", "hbs");
 
-app.use(express.static(publicDirectory))
+// tell the express where to look for template files other than default 'views' folder present at root folder
+app.set("views", viewsPath);
 
+// Setup static directory to serve
+app.use(express.static(publicDirectory));
 
-app.get('/', (req, res)=>{
-    res.render('index', {
-        title: 'Weather App',
-        name: "Saquib",
-    })
-})
+app.get("/", (req, res) => {
+	res.render("index", {
+		title: "Weather App",
+		name: "Saquib",
+	});
+});
 
-app.get('/about', (req, res)=>{
-    res.render('about', {
-        title: 'About Me',
-        name: "Saquib Asghar"
-    })
-})
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Me",
+		name: "Saquib Asghar",
+	});
+});
 
-app.get('/help', (req, res)=>{
-    res.render('help', {
-        help:'Help',
-        mess: "Some test message"
-    })
-})
+app.get("/help", (req, res) => {
+	res.render("help", {
+		help: "Help",
+		mess: "Some test message",
+	});
+});
 
 app.get("/weather", (req, res) => {
 	res.json([
@@ -59,7 +60,6 @@ app.get("/weather", (req, res) => {
 		},
 	]);
 });
-
 
 app.listen(port, () => {
 	console.log(`Server is running at port number ${port}`);
