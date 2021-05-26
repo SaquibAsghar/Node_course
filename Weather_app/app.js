540bb63a2ad2672870bea573bc805d6d035620ac
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-05-26 10:33:47 +0530

    Update app.js

--- Weather_app/app.js
+++ Weather_app/app.js
@@ -1,19 +1,10 @@
-const {getGeoCode} = require("./utils/geoLocation");
-const getWeather = require('./utils/getWeather')
+const getGeoInfo = require("./utils/geoLocation");
+const getWeatherForcast = require("./utils/getWeather");
 
-const callback = (mess, data) => {
-	if (mess) {
-		return mess;
-	}
-	return getWeather(data, weatherInfo);
-};
-const userSearchLoc = process.argv[2]
-getGeoCode(userSearchLoc, callback);
+const loc = process.argv[2];
 
-const weatherInfo = (mess, data) => {
-	if (mess) {
-		return console.log(mess)
-	}
-	return console.log(
-        `Weather: ${data.current.weather_descriptions[0]}. Currently it is ${data.current.temperature} C degree. It feels liks ${data.current.feelslike} C degree.`);
-};
+getGeoInfo(loc, (data) => {
+	getWeatherForcast(data, (data)=>{
+		console.log(data)
+	});
+});
