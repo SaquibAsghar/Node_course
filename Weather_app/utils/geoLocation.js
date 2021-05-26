3892cb536f9e66c77bc1477cf444496e407a57bd
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-05-26 10:34:12 +0530

    update code

--- Weather_app/utils/geoLocation.js
+++ Weather_app/utils/geoLocation.js
@@ -1,29 +1,41 @@
 const request = require("postman-request");
 
-const getGeoCode = (location, cb) => {
+const getGeoInfo = (location, cb) => {
 	const map_access_token =
 		"pk.eyJ1Ijoic2FxdWliYXNnaGFyLTE2IiwiYSI6ImNrcDJleXRpNDB4ZWoyb3RlNXBlaDEzNmsifQ.2FF5AHzXcZs9NqMXtEBsPw";
-	let limit = 1;
-	const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?access_token=${map_access_token}&limit=${limit}`;
+	const mapUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+		location
+	)}.json?access_token=${map_access_token}&limit=1`;
 
 	request({ url: mapUrl, json: true }, (err, res) => {
 		let message = "";
+		let status;
 		if (err) {
+			status = false;
 			message = "Something went wrong";
-			return cb(message, undefined);
-		} else if (res.body.features.length === 0) {
-            console.log(res.body)
-			message = "Can't find location. Try another location.";
-			return cb(message, undefined);
-		} else {
 			const data = {
-				longNlatt: res.body.features[0].center.reverse(),
-				place_name: res.body.features[0].place_name,
+				status,
+				message,
 			};
-
-			return cb(message, data);
+			return cb(data);
+		} else if (!res.body.features.length) {
+			status = false;
+			message = "Address not correct";
+			const data = {
+				status,
+				message,
+			};
+			return cb(data);
 		}
+		const [feature] = res.body.features;
+		const locData = {
+			status: true,
+			place_name: feature.place_name,
+			co_ordinate: feature.center.reverse(),
+		};
+
+		return cb(locData);
 	});
 };
 
-module.exports = { getGeoCode };
+module.exports = getGeoInfo;
--- Weather_app/utils/getWeather.js
+++ Weather_app/utils/getWeather.js
@@ -1,22 +1,34 @@
-const request = require('postman-request')
+const request = require("postman-request");
 
-const getWeather = ({longNlatt}, cb) => {
+const getWeatherForcast = (data, cb) => {
+	if (!data.status) {
+		return cb(data);
+	}
 	const url_access_key = "db83186412c5adec5a422f55b32ea9b6";
-	const url = `http://api.weatherstack.com/current?access_key=${url_access_key}&query=${longNlatt}`;
-    console.log(url)
-    let message = "";
-    request({url, json:true}, (err, res)=>{
-        if (err){
-            message = 'some err '
-            return cb(message, undefined)
-        }else if(res.body.success === false){
-            console.log(res.body)
-            message = "Provided location not valid"
-            return cb(message, undefined)
-        }
-        const data = res.body
-        return cb(message, data)
-    })
+	const url = `http://api.weatherstack.com/current?access_key=${url_access_key}&query=${data.co_ordinate}`;
+
+	request({ url, json: true }, (err, res) => {
+		let message = "";
+		let status;
+		if (err) {
+			status = false;
+			message = "Something went wrong";
+			const data = {
+				status,
+				message,
+			};
+
+			return cb(data);
+		}
+		const { location, current } = res.body;
+		status = true;
+		const data = {
+			status,
+			location,
+			current,
+		};
+		return cb(data);
+	});
 };
 
-module.exports = getWeather
+module.exports = getWeatherForcast;
