05986114aaee713fed751a8182667ffcf7f78ad6
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-05-26 14:49:23 +0530

    update files

--- web_server/static/public/js/main.js
+++ web_server/static/public/js/main.js
@@ -3,7 +3,8 @@ console.log("File is loaded on to clent side");
 const weatherForm = document.querySelector("form");
 const messageOne = document.querySelector(".status")
 const usrInp = document.querySelector("#searchLocation");
-
+const image = document.querySelector('img')
+console.log(image.src)
 const searchLocation = (name) => {
 	fetch(`http://127.0.0.1:3000/weather?location=${name}`)
 		.then((res) => {
@@ -13,7 +14,11 @@ const searchLocation = (name) => {
                     messageOne.textContent = data.message
 					return console.log(data.message);
 				}
-                messageOne.textContent = data.current.temperature
+                messageOne.textContent = data.current.temperature+ "  " 
+                // console.log(data.current.weather_icons[0])
+                // image.attributes[0] = data.current.weather_icons[0]
+                // console.log(image)
+
 				return console.log(data);
 			});
 		})
